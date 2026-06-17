d5a60907ef0d727f1a4b2e290427d99fd1d56973
jedokaplan <jed.kaplan@arve-research.com>
2026-06-18 05:18:14 +0800

    edits to work at native resolution and simplify operation

--- calcwetf.f90
+++ calcwetf.f90
@@ -1,6 +1,6 @@
 program calcwetf
 
-! gfortran -o calcwetf calcwetf.f90 -I/home/public/easybuild/software/netCDF-Fortran/4.6.1-gompi-2023a/include -lnetcdff
+! gfortran -Wall -o calcwetf calcwetf.f90 -I/home/public/easybuild/software/netCDF-Fortran/4.6.1-gompi-2023a/include -lnetcdff
 
 use iso_fortran_env
 use netcdf
@@ -27,8 +27,8 @@ integer :: tlen
 
 integer :: m
 
-real(dp), allocatable, dimension(:)     :: lon
-real(dp), allocatable, dimension(:)     :: lat
+! real(dp), allocatable, dimension(:)     :: lon
+! real(dp), allocatable, dimension(:)     :: lat
 real(sp), allocatable, dimension(:,:)   :: slope
 real(sp), allocatable, dimension(:,:)   :: intercept
 real(sp), allocatable, dimension(:,:,:) :: prec
@@ -37,8 +37,14 @@ real(sp), allocatable, dimension(:,:,:) :: wetf
 real(sp), dimension(2) :: actual_range
 
 ! -------------------------------------------------------
+! new version updated to estimate wet days on the native GCM grid
+! preprocess the statsfile with cdo remapmean to match the resolution of the GCM
 
-statsfile='/home/terraces/datasets/climate/CHELSA/wetVpre-linear-annual_filled.nc'
+call getarg(1,precfile)
+call getarg(2,statsfile)
+call getarg(3,wetffile)
+
+! -----
 
 status = nf90_open(statsfile,nf90_nowrite,ncid)
 if (status /= nf90_noerr) call handle_err(status)
@@ -74,7 +80,7 @@ status = nf90_close(ncid)
 if (status /= nf90_noerr) call handle_err(status)
 
 ! ----
-! NB the coefficients expect input in the form of monthly mean of daily precipitation rate
+! NB the coefficients expect input in the form of monthly mean of daily precipitation rate (mm day-1)
 
 call getarg(1,precfile)
 
@@ -112,20 +118,7 @@ wetf = max(wetf,0.)
 
 ! ----
 
-call getarg(2,wetffile)
-
 status = nf90_open(wetffile,nf90_write,ncid)
-if (status /= nf90_noerr) call handle_err(status)
-
-status = nf90_inq_varid(ncid,'prec',varid)
-if (status /= nf90_noerr) call handle_err(status)
-
-status = nf90_put_var(ncid,varid,prec)
-if (status /= nf90_noerr) call handle_err(status)
-
-actual_range = [minval(prec),maxval(prec)]
-
-status = nf90_put_att(ncid,varid,'actual_range',actual_range)
 if (status /= nf90_noerr) call handle_err(status)
 
 status = nf90_inq_varid(ncid,'wetf',varid)
@@ -134,10 +127,10 @@ if (status /= nf90_noerr) call handle_err(status)
 status = nf90_put_var(ncid,varid,wetf)
 if (status /= nf90_noerr) call handle_err(status)
 
-actual_range = [minval(prec),maxval(prec)]
-
-status = nf90_put_att(ncid,varid,'actual_range',actual_range)
-if (status /= nf90_noerr) call handle_err(status)
+! actual_range = [minval(prec),maxval(prec)]
+! 
+! status = nf90_put_att(ncid,varid,'actual_range',actual_range)
+! if (status /= nf90_noerr) call handle_err(status)
 
 status = nf90_close(ncid)
 if (status /= nf90_noerr) call handle_err(status)
--- makewetdays.sh
+++ makewetdays.sh
@@ -1,61 +1,44 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-# module load CDO
+module load CDO netCDF-Fortran
 
-historical=/home/terraces/datasets/CMIP5/historical/Amon
+srcdir=/home/terraces/datasets/CMIP6/historical/climatology
 
-paleo=/home/terraces/datasets/CMIP5/midHolocene/Amon
+if [ ! -e $srcdir/wetf ]
+then
+  mkdir -p $srcdir/wetf
+fi
 
-# ----
-# paleo - using 6ka
-
-# for infile in `ls $paleo/pr/*.nc`
-# do
-# 
-#   output=${infile##*/}
-# 
-#   echo $output
-#     
-#   # convert to mm mon-1 and interpolate to 30min 
-#   
-#   cdo -s -P 32 -f nc4 -z zip_1 -remapbic,global_0.5 -mulc,86400 -ymonmean $infile paleo/precip/$output
-#   
-#   # estimate wetdays
-#   
-#   ncgen -4 -o paleo/wetf/$output wetf.cdl
-#   
-#   ./calcwetf paleo/precip/$output paleo/wetf/$output
-# 
-# done
-# 
-# # ----
-# # historical - use 1971-2000
-# 
-# for infile in `ls $historical/pr/*.nc`
-# do
-# 
-#   output=${infile##*/}
-# 
-#   echo $output
-#     
-#   # convert to mm mon-1 and interpolate to 30min 
-#   
-#   cdo -s -P 32 -f nc4 -z zip_1 -remapbic,global_0.5 -mulc,86400 -ymonmean -seldate,1971-01-01,2000-12-31 $infile historical/precip/$output
-#   
-#   # estimate wetdays
-#   
-#   ncgen -4 -o  historical/wetf/$output wetf.cdl
-#   
-#   ./calcwetf historical/precip/$output historical/wetf/$output
-# 
-# done
-
-hist=historical/wetf
-paleo=paleo/wetf
-
-for model in CNRM-CM5 GISS-E2-R HadGEM2-ES IPSL-CM5A-L MIROC-ESM MPI-ESM-P MRI-CGCM3
+for infile in `ls $srcdir/pr/pr*.nc`   # file containing precipitation is the input file
 do
 
-  cdo sub $paleo/pr_Amon_$model*.nc $hist/pr_Amon_$model*.nc anomalies/$model"_"wetf_anom.nc
+  output=$srcdir/wetf/wetf${infile##*pr}
+  
+  echo $infile
+  echo $output
+
+  # create output file with the same dimensions as input
+  
+  ncdump -cs $infile | ncgen -4 -o tmp.nc
+  
+  cdo -f nc4 setpartabn,wetfrac.namelist tmp.nc $output
+  
+  rm tmp.nc
+  
+  # remap stats file to size of input
+  
+  cdo -f nc4 -P 40 remapmean,$infile wetVpre-linear-annual_filled.nc stats.nc
+  
+  # convert input precip to mm day-1
+  
+  cdo -f nc4 mulc,86400 $infile input.nc
+  
+  # calculate wet day fraction
+  
+  ./calcwetf input.nc stats.nc $output
+
+  # clean up
+
+  rm input.nc stats.nc
 
 done
